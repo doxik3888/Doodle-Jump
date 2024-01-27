9da148caece57aa8c16723006ce5a260e2aac88e
doxik3888 <153354996+doxik3888@users.noreply.github.com>
2024-01-27 13:31:01 +0300

    Add files via upload

--- Doodle-Jump-main/scripts/app.py
+++ Doodle-Jump-main/scripts/app.py
@@ -1,29 +1,28 @@
 import pygame
-import os
+
 from scripts.game import Game
-from scripts.functions import load_image
+from scripts.functions import load_image 
+from scripts.constants import display_size
 
 class App:
-    def __init__(self) :
-        self.display_size = (480, 720)
+    def __init__(self):
         self.running = True
         self.maxFPS = 60
 
-        self.display = pygame.display.set_mode(self.display_size)
+        self.display = pygame.display.set_mode(display_size)
         self.clock = pygame.time.Clock()
         self.game = Game()
 
-        pygame.display.set_caption('Doodle Jump')
-        pygame.display.set_icon(load_image("assets","icons","icon.ico"))
+        pygame.display.set_caption('Dodle Jump')
+        pygame.display.set_icon(load_image('assets','icons','icon.ico'))
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
-
-            elif event.type == pygame.KEYDOWN :
+                self.running = False 
+            
+            elif event.type == pygame.KEYDOWN:
                 self.game.handle_key_down_event(event.key)
-
             elif event.type == pygame.KEYUP:
                 self.game.handle_key_up_event(event.key)
 
@@ -40,5 +39,4 @@ class App:
             self.handle_events()
             self.update()
             self.render()
-
             self.clock.tick(self.maxFPS)
--- Doodle-Jump-main/scripts/game.py
+++ Doodle-Jump-main/scripts/game.py
@@ -1,43 +1,44 @@
 import pygame
 import os
+from scripts.constants import display_size
+from scripts.player import Player
 from scripts.functions import load_image
 from scripts.sprite import Sprite
-from scripts.player import Player
 from scripts.platform import Platform
 
-class Game:
-    def __init__(self):
-        self.background = load_image("assets","images","background.png")
-        self.player = Player((200,600),load_image('assets','images','player.png'),5, 20, 0.65 ) 
-        self.platforms = [
-            Platform((240, 700), load_image('assets', 'images', 'platform.png')),
-            Platform((100, 450), load_image('assets', 'images', 'platform.png')),
-            Platform((400, 200), load_image('assets', 'images', 'platform.png'))
+class Game():
+    def __init__(self) -> None:
+        self.background_image = load_image('assets','images', 'background.png')
+        self.offset_y = 0
+        
+        self.player = Player((200, 200), load_image('assets', 'images', 'player.png'),5,20,0.65)
+        self.platforms = [ 
+            Platform((240, 700), load_image("assets", "images", "platform.png")),
+            Platform((100, 500), load_image("assets", "images", "platform.png")),
+            Platform((400, 300), load_image("assets", "images", "platform.png")),
         ]
+    def render(self, surface):
+        surface.blit(self.background_image, (0, 0))
+        for platform in self.platforms:
+            platform.render(surface, self.offset_y)
+        self.player.render(surface, self.offset_y)
 
 
-    def render(self,surface: pygame.Surface):
-        surface.blit(self.background,(0,0))
-        surface.blit(self.background_image, (0,0))
 
-        for platform in self.platform:
-            platform.render(surface)
-        self.player.render(surface)
-    
-    def handle_key_down_event(self,key):
-        if key == pygame.K_a :
-            self.player.is_walking_left = True
+    def handle_key_down_event(self, key):
+        if key == pygame.K_a:
+            self.player.is_walkind_left = True
         elif key == pygame.K_d:
             self.player.is_walking_right = True
-    def handle_key_up_event(self,key):
-        if key == pygame.K_a :
-            self.player.is_walking_left = False
+    def handle_key_up_event(self, key):
+        if key == pygame.K_a:
+            self.player.is_walkind_left = False
         elif key == pygame.K_d:
             self.player.is_walking_right = False
-
     def update(self):
         self.player.update()
-        
         for platform in self.platforms:
-            if self.player.collide_sprite():
+            if self.player.collide_sprite(platform):
                 self.player.on_platform = True
+        if self.player.rect.bottom - self.offset_y < display_size[1] / 3:
+            self.offset_y = self.player.rect.bottom - display_size[1] / 3
